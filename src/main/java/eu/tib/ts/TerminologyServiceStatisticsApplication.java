91458eace498bfcd08fdcb9a224b29763ed54ca3
Natalia Andrianova <natalia.andrianova@tib.eu>
2021-12-29 10:41:41 +0100

    init

---
+++ src/main/java/eu/tib/ts/TerminologyServiceStatisticsApplication.java
@@ -0,0 +1,13 @@
+package eu.tib.ts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminologyServiceStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminologyServiceStatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/tib/ts/TerminologyServiceStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.tib.ts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminologyServiceStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
