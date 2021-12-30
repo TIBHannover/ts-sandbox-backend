768057e1c6b483956a27d7897d35c4dd6eca1499
Natalia Andrianova <natalia.andrianova@tib.eu>
2021-12-30 17:05:56 +0100

    update config

--- src/main/java/eu/tib/ts/configuration/RestTemplateConfiguration.java
+++ src/main/java/eu/tib/ts/configuration/RestTemplateConfiguration.java
@@ -1,14 +1,17 @@
 package eu.tib.ts.configuration;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -23,12 +26,26 @@ public class RestTemplateConfiguration {
     private MappingJackson2HttpMessageConverter messageConverter;
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate(getClientHttpRequestFactory());
+        restTemplate.setMessageConverters(List.of(messageConverter));
 
-        return builder
-            .setConnectTimeout(Duration.ofMillis(connectTimeout))
-            .setReadTimeout(Duration.ofMillis(readTimeout))
-            .additionalMessageConverters(List.of(messageConverter))
-            .build();
+        return restTemplate;
     }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(connectTimeout)
+            .setConnectionRequestTimeout(connectTimeout)
+            .setSocketTimeout(connectTimeout)
+            .build();
+
+        CloseableHttpClient client = HttpClientBuilder
+            .create()
+            .setDefaultRequestConfig(config)
+            .build();
+
+        return new HttpComponentsClientHttpRequestFactory(client);
+    }
+
 }
