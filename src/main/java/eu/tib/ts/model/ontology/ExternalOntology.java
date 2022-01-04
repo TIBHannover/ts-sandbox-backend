33b12bcd6f2e2d166842f65e00d029b2561e6661
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-01-04 19:14:59 +0100

    ontology entities

---
+++ src/main/java/eu/tib/ts/model/ontology/ExternalOntology.java
@@ -0,0 +1,19 @@
+package eu.tib.ts.model.ontology;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Set;
+
+@Builder
+@Value
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ExternalOntology implements ExtendedOntology {
+    String ontologyId;
+    String uri;
+    Set<String> properties;
+    Set<String> classes;
+    Set<String> imports;
+    Set<String> namespaces;
+}
