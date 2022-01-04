4f223394424a6821abe9fb029955ae89448e7f10
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-01-04 19:13:47 +0100

    improve

--- src/main/java/eu/tib/ts/model/ontology/TsOntology.java
+++ src/main/java/eu/tib/ts/model/ontology/TsOntology.java
@@ -1,11 +1,18 @@
 package eu.tib.ts.model.ontology;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Value;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
 
 @Builder
 @Value
-public class TsOntology {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TsOntology implements Ontology {
     String ontologyId;
     String loaded;
     String updated;
@@ -18,4 +25,16 @@ public class TsOntology {
     int numberOfProperties;
     int numberOfIndividuals;
     Config config;
+
+    public Set<String> getCollection() {
+        return Objects.isNull(config) || CollectionUtils.isEmpty(config.getClassifications())
+            ? Collections.emptySet()
+            : Set.copyOf(config.getClassifications().get(0).getCollection());
+    }
+
+    public String getUri() {
+        return Objects.isNull(config)
+            ? null
+            : config.getFileLocation();
+    }
 }
