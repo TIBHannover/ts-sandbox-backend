7816ae7219a64a0f28000480a2846812538883ce
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-01-07 11:52:22 +0100

    remove public

--- src/test/java/eu/tib/ts/service/impl/OntologyReadServiceImplTest.java
+++ src/test/java/eu/tib/ts/service/impl/OntologyReadServiceImplTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class OntologyReadServiceImplTest {
+class OntologyReadServiceImplTest {
     private OntologyReadService ontologyReadService;
 
     @BeforeEach
--- src/test/java/eu/tib/ts/service/impl/SimilarityServiceImplTest.java
+++ src/test/java/eu/tib/ts/service/impl/SimilarityServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class SimilarityServiceImplTest {
+class SimilarityServiceImplTest {
     private SimilarityService similarityService;
     @Mock
     private ProcessedOntologyRepository processedOntologyRepository;
