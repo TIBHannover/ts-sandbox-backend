ec2afdd7bd321f03ff237b572a4a8da995f9cef7
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-01-24 21:51:20 +0100

    added tests

--- src/test/java/eu/tib/ts/service/impl/ProcessedOntologyServiceTest.java
+++ src/test/java/eu/tib/ts/service/impl/ProcessedOntologyServiceTest.java
@@ -1,5 +1,6 @@
 package eu.tib.ts.service.impl;
 
+import eu.tib.ts.controller.dto.OntologyDto;
 import eu.tib.ts.model.ontology.ProcessedOntology;
 import eu.tib.ts.repository.ProcessedOntologyRepository;
 import eu.tib.ts.service.ProcessedOntologyService;
@@ -9,11 +10,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ProcessedOntologyServiceTest {
+class ProcessedOntologyServiceTest extends TestData {
     @Mock
     private ProcessedOntologyRepository repository;
     private ProcessedOntologyService processedOntologyService;
@@ -41,5 +46,33 @@ class ProcessedOntologyServiceTest {
 
         verify(repository, times(1))
             .save(processedOntology);
+    }
+
+    @Test
+    void testGetOntologies() {
+        Iterable<ProcessedOntology> processedOntologies = getProcessedOntologies();
+        when(repository.findAll())
+            .thenReturn(processedOntologies);
+
+        List<OntologyDto> actual = processedOntologyService.getOntologies();
+
+        assertEquals(3, actual.size());
+        assertEquals("ontology_0", actual.get(0).getOntologyId());
+        assertEquals("ontology_1", actual.get(1).getOntologyId());
+        assertEquals("ontology_2", actual.get(2).getOntologyId());
+    }
+
+    @Test
+    void testGetOntologyIds() {
+        Iterable<ProcessedOntology> processedOntologies = getProcessedOntologies();
+        when(repository.findAll())
+            .thenReturn(processedOntologies);
+
+        List<String> actual = processedOntologyService.getOntologyIds();
+
+        assertEquals(3, actual.size());
+        assertEquals("ontology_0", actual.get(0));
+        assertEquals("ontology_1", actual.get(1));
+        assertEquals("ontology_2", actual.get(2));
     }
 }
