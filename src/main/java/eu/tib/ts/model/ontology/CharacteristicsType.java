744d16b535068d130c51eb7cf72e2953c39c8ebb
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-01 09:52:01 +0100

    added individuals to CharacteristicsType

--- src/main/java/eu/tib/ts/model/ontology/CharacteristicsType.java
+++ src/main/java/eu/tib/ts/model/ontology/CharacteristicsType.java
@@ -1,6 +1,7 @@
 package eu.tib.ts.model.ontology;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum CharacteristicsType {
     PROPERTY {
@@ -26,7 +27,19 @@ public enum CharacteristicsType {
         public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
             return ontology.getImports();
         }
+    },
+    INDIVIDUAL {
+        @Override
+        public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+            return ontology.getIndividuals();
+        }
     };
 
     public abstract <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology);
+
+    public <T extends ExtendedOntology> Set<String> getCharacteristicsToLowerCase(T ontology) {
+        return getCharacteristics(ontology).stream()
+            .map(String::toLowerCase)
+            .collect(Collectors.toSet());
+    }
 }
