c45e7b46754ab1f6487c3ca8fd6b66790faae064
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-01 09:35:10 +0100

    fixed pageable fields

--- src/main/java/eu/tib/ts/configuration/SwaggerConfiguration.java
+++ src/main/java/eu/tib/ts/configuration/SwaggerConfiguration.java
@@ -1,7 +1,11 @@
 package eu.tib.ts.configuration;
 
+import io.swagger.annotations.ApiParam;
+import lombok.Getter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.lang.Nullable;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,6 +20,28 @@ public class SwaggerConfiguration {
             .select()
             .paths(PathSelectors.any())
             .apis(RequestHandlerSelectors.basePackage("eu.tib.ts.controller"))
-            .build();
+            .build()
+            .directModelSubstitute(Pageable.class, SwaggerPageable.class);
+    }
+
+    @Getter
+    private static class SwaggerPageable {
+
+        @ApiParam(value = "Number of records per page", example = "0")
+        @Nullable
+        private Integer size;
+
+        @ApiParam(value = "Results page you want to retrieve (0..N)", example = "0")
+        @Nullable
+        private Integer page;
+
+        @ApiParam(value = "Sorting criteria in the format: property(,asc|desc). " +
+            "Default sort order is ascending. Multiple sort criteria are supported." +
+            "Example: sort=name,asc&sort=age,desc")
+        @Nullable
+        private String sort;
+
     }
 }
+
+
