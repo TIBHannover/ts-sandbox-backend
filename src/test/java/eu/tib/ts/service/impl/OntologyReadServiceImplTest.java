ad47825def33f03f110b38ec0f770279c38c8ac1
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-02 07:21:48 +0100

    read ontologies with two APIs

--- src/test/java/eu/tib/ts/service/impl/OntologyReadServiceImplTest.java
+++ src/test/java/eu/tib/ts/service/impl/OntologyReadServiceImplTest.java
@@ -1,10 +1,13 @@
 package eu.tib.ts.service.impl;
 
 import eu.tib.ts.service.OntologyReadService;
+import lombok.SneakyThrows;
 import org.apache.jena.ontology.OntModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,16 +20,31 @@ class OntologyReadServiceImplTest {
     }
 
     @Test
-    void testReadOntology_shouldReturnOntModel() {
-        OntModel ontModel = ontologyReadService.readOntology("http://purl.obolibrary.org/obo/rex.owl");
+    void testReadOntologyWithOwlApi_shouldReturnOWLOntology() throws OWLOntologyCreationException {
+        OWLOntology owlOntology = ontologyReadService.readOntologyWithOwlApi("http://purl.obolibrary.org/obo/rex.owl");
+        assertNotNull(owlOntology);
+    }
+
+    @SneakyThrows
+    @Test
+    void testReadOntologyWithJenaApi_shouldReturnOntModel() {
+        OntModel ontModel = ontologyReadService.readOntologyWithJenaApi("http://purl.obolibrary.org/obo/rex.owl");
         assertNotNull(ontModel);
     }
 
     @Test
-    void testReadOntology_shouldThrowException() {
+    void testReadOntologyWithJenaApi_shouldThrowException() {
 
-        Assertions.assertThrows(org.apache.jena.atlas.web.HttpException.class, () ->
-            ontologyReadService.readOntology("http://test.owl")
+        Assertions.assertThrows(Exception.class, () ->
+            ontologyReadService.readOntologyWithJenaApi("http://test.owl")
+        );
+    }
+
+    @Test
+    void testReadOntologyWithOwlApi_shouldThrowException() {
+
+        Assertions.assertThrows(Exception.class, () ->
+            ontologyReadService.readOntologyWithOwlApi("http://test.owl")
         );
     }
 
