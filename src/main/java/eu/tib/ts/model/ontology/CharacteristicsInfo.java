f41a5ee96f3f8fc7ecbc67cacb89c2c68a53ac75
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-02 11:47:52 +0100

    added percent

--- src/main/java/eu/tib/ts/model/ontology/CharacteristicsInfo.java
+++ src/main/java/eu/tib/ts/model/ontology/CharacteristicsInfo.java
@@ -9,11 +9,15 @@ import java.util.List;
 @Value
 public class CharacteristicsInfo {
     long size;
+    long maxSimilaritiesSize;
+    double percent;
     List<String> list;
 
-    public static CharacteristicsInfo of(List<String> similarities) {
+    public static CharacteristicsInfo of(List<String> similarities, long maxSimilaritiesSize, double percent) {
         return CharacteristicsInfo.builder()
             .size(similarities.size())
+            .maxSimilaritiesSize(maxSimilaritiesSize)
+            .percent(percent)
             .list(similarities)
             .build();
     }
