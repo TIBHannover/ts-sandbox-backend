8bff4079d1446ab30d44f67696e86f414c3618d9
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-02 07:10:30 +0100

    improved

--- src/main/java/eu/tib/ts/model/ontology/CharacteristicsType.java
+++ src/main/java/eu/tib/ts/model/ontology/CharacteristicsType.java
@@ -1,44 +1,54 @@
 package eu.tib.ts.model.ontology;
 
+import org.springframework.util.CollectionUtils;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum CharacteristicsType {
     PROPERTY {
         @Override
-        public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+        public <T extends ExtendedOntology> Set<String> getOntologyCharacteristics(T ontology) {
             return ontology.getProperties();
         }
     },
     CLASS {
         @Override
-        public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+        public <T extends ExtendedOntology> Set<String> getOntologyCharacteristics(T ontology) {
             return ontology.getClasses();
         }
     },
     NAMESPACE {
         @Override
-        public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+        public <T extends ExtendedOntology> Set<String> getOntologyCharacteristics(T ontology) {
             return ontology.getNamespaces();
         }
     },
     IMPORT {
         @Override
-        public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+        public <T extends ExtendedOntology> Set<String> getOntologyCharacteristics(T ontology) {
             return ontology.getImports();
         }
     },
     INDIVIDUAL {
         @Override
-        public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+        public <T extends ExtendedOntology> Set<String> getOntologyCharacteristics(T ontology) {
             return ontology.getIndividuals();
         }
     };
 
-    public abstract <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology);
+    public abstract <T extends ExtendedOntology> Set<String> getOntologyCharacteristics(T ontology);
+
+    public <T extends ExtendedOntology> Set<String> getCharacteristics(T ontology) {
+        Set<String> ontologyCharacteristics = getOntologyCharacteristics(ontology);
+
+        return CollectionUtils.isEmpty(ontologyCharacteristics)
+            ? Set.of()
+            : ontologyCharacteristics;
+    }
 
     public <T extends ExtendedOntology> Set<String> getCharacteristicsToLowerCase(T ontology) {
-        return getCharacteristics(ontology).stream()
+        return getOntologyCharacteristics(ontology).stream()
             .map(String::toLowerCase)
             .collect(Collectors.toSet());
     }
