2ffaf1ec33dfcd829b97b24baf2e13a909daaf3b
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-07 10:20:49 +0100

    changed ontology parameter type

--- src/main/java/eu/tib/ts/service/ChartService.java
+++ src/main/java/eu/tib/ts/service/ChartService.java
@@ -2,7 +2,7 @@ package eu.tib.ts.service;
 
 import eu.tib.ts.model.chart.ChartData;
 import eu.tib.ts.model.chart.ChartRequest;
-import eu.tib.ts.model.ontology.ExternalOntology;
+import eu.tib.ts.model.ontology.ExtendedOntology;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -11,5 +11,5 @@ import java.util.Optional;
 public interface ChartService {
     ChartData chart(Optional<List<String>> ids, Optional<String> collection, ChartRequest request, Pageable pageable);
 
-    ChartData chart(ExternalOntology ontology, Optional<String> collection, ChartRequest request, Pageable pageable);
+    <T extends ExtendedOntology> ChartData chart(T ontology, Optional<String> collection, ChartRequest request, Pageable pageable);
 }
