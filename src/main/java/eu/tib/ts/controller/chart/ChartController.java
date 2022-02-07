bb3efa0f8c6ae6e6a9139378879a3228a57d6c3b
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-07 10:22:29 +0100

    changed to external ontology by URL

--- src/main/java/eu/tib/ts/controller/chart/ChartController.java
+++ src/main/java/eu/tib/ts/controller/chart/ChartController.java
@@ -2,8 +2,9 @@ package eu.tib.ts.controller.chart;
 
 import eu.tib.ts.model.chart.ChartData;
 import eu.tib.ts.model.chart.ChartRequest;
-import eu.tib.ts.model.ontology.ExternalOntology;
+import eu.tib.ts.model.ontology.ProcessedOntology;
 import eu.tib.ts.service.ChartService;
+import eu.tib.ts.service.PreProcessingOntologyService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +23,17 @@ import java.util.Optional;
 @RequestMapping("/api/ontology/similarity/pairwise/chart")
 public class ChartController {
     private final ChartService chartService;
+    private final PreProcessingOntologyService preProcessingOntologyService;
 
     @Autowired
-    public ChartController(ChartService chartService) {
+    public ChartController(ChartService chartService,
+                           PreProcessingOntologyService preProcessingOntologyService) {
         this.chartService = chartService;
+        this.preProcessingOntologyService = preProcessingOntologyService;
     }
 
     @ApiOperation(value = "Pairwise similarity Bar Chart between TS internal ontologies")
-    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/internal", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<byte[]> chart(
         Pageable pageable,
         @RequestParam(required = false) Optional<List<String>> ids,
@@ -55,12 +57,11 @@ public class ChartController {
     }
 
     @ApiOperation("Pairwise similarity Bar Chart for external ontology")
-    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/external", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<byte[]> chartForExternalOntology(
-        @ApiParam(value = "External ontology")
-        @RequestBody ExternalOntology ontology,
         Pageable pageable,
-        @RequestParam(required = false) Optional<List<String>> ids,
+        @ApiParam(value = "External ontology URL")
+        @RequestParam String url,
         @RequestParam(required = false) Optional<String> collection,
         @RequestParam(required = false) Optional<Boolean> horizontal,
         @RequestParam(required = false) Optional<Integer> height,
@@ -72,6 +73,7 @@ public class ChartController {
             .horizontal(horizontal)
             .build();
 
+        ProcessedOntology ontology = preProcessingOntologyService.preProcess(Optional.empty(), url);
         ChartData chartData = chartService.chart(ontology, collection, request, pageable);
 
         return ResponseEntity.ok()
