1f037bba82f3994fc0b27a0c1fdc421f4c754683
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-23 13:35:15 +0100

    added ontology id

--- src/main/java/eu/tib/ts/controller/dto/OntologyDto.java
+++ src/main/java/eu/tib/ts/controller/dto/OntologyDto.java
@@ -7,11 +7,13 @@ import lombok.Value;
 @Builder
 @Value
 public class OntologyDto {
+    long id;
     String ontologyId;
     String uri;
 
     public static OntologyDto of(ProcessedOntology processedOntology) {
         return OntologyDto.builder()
+            .id(processedOntology.getId())
             .ontologyId(processedOntology.getOntologyId())
             .uri(processedOntology.getUri())
             .build();
