7c8990e9729f751cc472058ba623e76eb48d6979
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-05-24 14:31:34 +0200

    added @ControllerAdvice

---
+++ src/main/java/eu/tib/ts/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,75 @@
+package eu.tib.ts.controller.exception;
+
+import eu.tib.ts.model.error.ErrorResponse;
+import eu.tib.ts.repository.exception.TsRepositoryException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    public static final String ERROR_OCCURRED = "An error occurred: ";
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
+        log.error(ERROR_OCCURRED, ex);
+
+        var response = ErrorResponse.builder()
+            .status(HttpStatus.BAD_REQUEST.value())
+            .error(ex.getLocalizedMessage())
+            .message("Client error")
+            .path(request.getRequestURI())
+            .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = TsRepositoryException.class)
+    ResponseEntity<ErrorResponse> handleTsRepositoryException(TsRepositoryException ex, HttpServletRequest request) {
+        log.error(ERROR_OCCURRED, ex);
+
+        var response = ErrorResponse.builder()
+            .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .error(ex.getLocalizedMessage())
+            .message("Server error")
+            .path(request.getRequestURI())
+            .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
+        log.error(ERROR_OCCURRED, ex);
+
+        var response = ErrorResponse.builder()
+            .status(HttpStatus.NOT_FOUND.value())
+            .error(ex.getLocalizedMessage())
+            .message("Not found")
+            .path(request.getRequestURI())
+            .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAll(Exception ex, HttpServletRequest request) {
+        log.error(ERROR_OCCURRED, ex);
+
+        var response = ErrorResponse.builder()
+            .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .error(ex.getLocalizedMessage())
+            .message("Server error")
+            .path(request.getRequestURI())
+            .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
