bdc1d0fa74c245c1ddb08f8c86b2b29cb797fd62
nenadkrdzavac <nenad.krdzavac@gmail.com>
2023-04-21 17:11:08 +0200

    added pre processing mapping service impl.

---
+++ src/main/java/eu/tib/ts/service/impl/PreProcessingMappingServiceImpl.java
@@ -0,0 +1,92 @@
+package eu.tib.ts.service.impl;
+
+import eu.tib.ts.model.ontology.ProcessedMapping;
+import eu.tib.ts.model.ontology.ProcessedOntology;
+import eu.tib.ts.model.ontology.TsOntology;
+import eu.tib.ts.service.OntologyReadService;
+import eu.tib.ts.service.OntologyTraverseService;
+import eu.tib.ts.service.PreProcessingMappingService;
+import eu.tib.ts.service.PreProcessingOntologyService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.ontology.OntModel;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import uk.ac.ox.krr.logmap2.mappings.objects.MappingObjectStr;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class PreProcessingMappingServiceImpl implements PreProcessingMappingService {
+
+    public static final String EXTERNAL = "external";
+//    private final OntologyReadService ontologyReadService;
+//    private final OntologyTraverseService ontologyTraverseService;
+
+    long idd = 0;
+
+    @Override
+    public ProcessedMapping preProcess(Set<MappingObjectStr> MappingObjectStrSet, OWLOntology sourceOntologyIRI, OWLOntology targetOntologyIRI) {
+        return null;
+    }
+
+//    @Autowired
+//    public PreProcessingMappingServiceImpl(OntologyReadService ontologyReadService,
+//                                           OntologyTraverseService ontologyTraverseService) {
+//        this.ontologyReadService = ontologyReadService;
+//        this.ontologyTraverseService = ontologyTraverseService;
+//
+//    }
+
+//    @Override
+//    public ProcessedOntology preProcess(Optional<TsOntology> tsOntology, String fileLocation, String title) {
+//
+//        log.debug("Start pre-processing {} {}", tsOntology.map(TsOntology::getOntologyId).orElse(EXTERNAL), fileLocation);
+//
+//        OntModel ontModel = null;
+//        OWLOntology owlOntology = null;
+//
+//        try {
+//            ontModel = ontologyReadService.readOntologyWithJenaApi(fileLocation);
+//        } catch (Exception e) {
+//            log.error("Could not read with Jena API {} {}", fileLocation, e.getLocalizedMessage());
+//        }
+//
+//        try {
+//            owlOntology = ontologyReadService.readOntologyWithOwlApi(fileLocation);
+//        } catch (Exception e) {
+//            log.error("Could not read with OWL API {} {}", fileLocation, e.getLocalizedMessage());
+//        }
+//
+//        return buildOntology(tsOntology, owlOntology, ontModel, fileLocation, title);
+//
+//    }
+//
+//    private ProcessedOntology buildOntology(Optional<TsOntology> tsOntology,
+//                                            OWLOntology owlOntology,
+//                                            OntModel ontModel,
+//                                            String uri,
+//                                            String titled) {
+//
+//        Set<String> classes = ontologyTraverseService.getClasses(ontModel);
+//        if (CollectionUtils.isEmpty(classes)) {
+//            classes = ontologyTraverseService.getClasses(owlOntology);
+//        }
+//
+//        return ProcessedOntology.builder()
+//                .ontologyId(tsOntology.map(TsOntology::getOntologyId).orElse(EXTERNAL))
+//                .classes(classes)
+//                .imports(CollectionUtils.isEmpty(ontologyTraverseService.getImports(owlOntology)) ? Collections.emptySet():ontologyTraverseService.getImports(owlOntology))
+//                .properties(CollectionUtils.isEmpty(ontologyTraverseService.getProperties(ontModel)) ? Collections.emptySet():ontologyTraverseService.getProperties(ontModel))
+//                .namespaces(CollectionUtils.isEmpty(ontologyTraverseService.getNamespaces(owlOntology)) ? Collections.emptySet():ontologyTraverseService.getNamespaces(owlOntology))
+//                .individuals(CollectionUtils.isEmpty(ontologyTraverseService.getIndividuals(owlOntology)) ? Collections.emptySet():ontologyTraverseService.getIndividuals(owlOntology))
+//                .title(titled)
+//                .collection(tsOntology.map(TsOntology::getCollection).orElse(Collections.emptySet()))
+//                .uri(uri)
+//                .build();
+//    }
+}
