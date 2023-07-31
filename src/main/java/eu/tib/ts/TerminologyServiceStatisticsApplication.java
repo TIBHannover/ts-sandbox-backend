85c317f8e08e114aa2b874a2ac6c534a718f2a87
Aamir Muhammad <aamir.muhammad@tib.eu>
2023-07-31 18:17:49 +0200

    Check service for all ontologies

--- src/main/java/eu/tib/ts/TerminologyServiceStatisticsApplication.java
+++ src/main/java/eu/tib/ts/TerminologyServiceStatisticsApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.*;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableAutoConfiguration
-//@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class TerminologyServiceStatisticsApplication {
 
     public static void main(String[] args) {
--- src/main/java/eu/tib/ts/configuration/WebConfiguration.java
+++ src/main/java/eu/tib/ts/configuration/WebConfiguration.java
@@ -16,6 +16,12 @@ public class WebConfiguration  implements WebMvcConfigurer  {
         registry.addConverter(new StringToEnumConverter());
     }
 
+    /**
+     * @author Aamir Muhammad <aamir.muhhamad@tib.eu>
+     *     To remove CORS origin issue we created a method that adds cors mappings using cors registry.
+     *     WebConfiguration class does not extend WebMvcConfigurerAdapter.
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
