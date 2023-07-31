efa45c08c4328346b66068f5d00222ee46a945d2
Aamir Muhammad <aamir.muhammad@tib.eu>
2023-07-31 17:52:31 +0200

    Adding web configuration for CORS

--- src/main/java/eu/tib/ts/TerminologyServiceStatisticsApplication.java
+++ src/main/java/eu/tib/ts/TerminologyServiceStatisticsApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableAutoConfiguration
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+//@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class TerminologyServiceStatisticsApplication {
 
     public static void main(String[] args) {
--- src/main/java/eu/tib/ts/configuration/WebConfiguration.java
+++ src/main/java/eu/tib/ts/configuration/WebConfiguration.java
@@ -3,13 +3,22 @@ package eu.tib.ts.configuration;
 import eu.tib.ts.converter.StringToEnumConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+@EnableWebMvc
+public class WebConfiguration  implements WebMvcConfigurer  {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 
