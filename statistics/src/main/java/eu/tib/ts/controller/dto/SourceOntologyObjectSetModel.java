8004e1946fbc517a626b49981de980b169b1f879
nenadkrdzavac <nenad.krdzavac@gmail.com>
2023-09-22 09:45:27 +0200

    updated preprocessing service impl

--- statistics/src/main/java/eu/tib/ts/controller/dto/SourceOntologyObjectSetModel.java
+++ statistics/src/main/java/eu/tib/ts/controller/dto/SourceOntologyObjectSetModel.java
@@ -9,14 +9,12 @@ import java.util.Set;
 @Setter
 public class SourceOntologyObjectSetModel {
 
-//    OntologyDto sourceOntology;
     long id;
     String ontologyId;
     String uri;
     String title;
     /*
-    added 01.03.2023.
-    ontology collection
+     * ontology collection
      */
     Set<String> collection;
 
