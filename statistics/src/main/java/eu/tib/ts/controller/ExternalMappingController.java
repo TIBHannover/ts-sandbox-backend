cbac01bf22cad163b8a3fe87c81311601a4f0cde
nenadkrdzavac <nenad.krdzavac@gmail.com>
2023-11-22 16:55:42 +0100

    updatedd external mapping controller

--- statistics/src/main/java/eu/tib/ts/controller/ExternalMappingController.java
+++ statistics/src/main/java/eu/tib/ts/controller/ExternalMappingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/ontology/external/mapping")
+@RequestMapping("api/ontology/external")
 public class ExternalMappingController {
 
 
