b39c14b817ed0f6e2c0f6347af014c1fa0fabe20
nenadkrdzavac <nenad.krdzavac@gmail.com>
2023-11-22 15:58:25 +0100

    added external mapping controller class

---
+++ statistics/src/main/java/eu/tib/ts/controller/ExternalMappingController.java
@@ -0,0 +1,10 @@
+package eu.tib.ts.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/ontology")
+public class ExternalMappingController {
+
+}
