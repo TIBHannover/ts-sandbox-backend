1fa274a68a7ae95cc857d733b0b7f57fdc5861c0
nenadkrdzavac <nenad.krdzavac@gmail.com>
2023-12-15 11:41:07 +0100

    changed from toList to collectio to list

--- statistics/src/main/java/eu/tib/ts/service/impl/RatioServiceImpl.java
+++ statistics/src/main/java/eu/tib/ts/service/impl/RatioServiceImpl.java
@@ -33,7 +33,7 @@ public class RatioServiceImpl implements RatioService {
 
         List<Set<String>> characteristics = processedOntologies.stream()
             .map(characteristicsType::getCharacteristics)
-            .toList();
+            .collect(Collectors.toList());
 
         double distinctCharacteristicsNumber = characteristics.stream()
             .flatMap(Collection::stream)
@@ -54,7 +54,7 @@ public class RatioServiceImpl implements RatioService {
     private <T extends Ontology> List<ProcessedOntology> getProcessedOntologies(List<T> ontologies) {
         List<String> ids = ontologies.stream()
             .map(Ontology::getOntologyId)
-            .toList();
+                .collect(Collectors.toList());   ;
 
         return ProcessedMongoOntologyRepository.findByOntologyIdIn(ids);
     }
