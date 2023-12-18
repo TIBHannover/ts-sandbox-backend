a1972b59e90aec62ed8ea278e27d122aa54feefe
Aamir <aamir.muhammad@tib.eu>
2023-12-18 11:11:31 +0100

    update on mongodb inconsistence operation

--- statistics/src/main/java/eu/tib/ts/service/impl/UpdateGitMedias.java
+++ statistics/src/main/java/eu/tib/ts/service/impl/UpdateGitMedias.java
@@ -5,25 +5,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component
 public class UpdateGitMedias {
     @Autowired
     private GitPreProcessingService preProcessingService;
 
-    @Scheduled(fixedDelay = 7 * 86400000)
+    @Scheduled(initialDelay = 7 * 86400000, fixedDelay = 7 * 86400000)
     public void doSomething() {
 
-        System.out.println("executing once in a week");
+        System.out.println("I am execting after one week");
 
-        try{
-            TimeUnit.SECONDS.sleep(25);
+
+        // Create a Runnable object
+        Runnable myRunnable = new MyRunnable();
+
+        // Create a thread and associate it with the Runnable
+        Thread thread = new Thread(myRunnable);
+
+        // Start the thread
+        thread.start();
+
+
+    }
+
+    class MyRunnable implements Runnable {
+        public void run() {
             preProcessingService.doGitPreProcessing();
-            }
-        catch(InterruptedException e) {
-            throw new RuntimeException(e);
         }
+
     }
 
 }
