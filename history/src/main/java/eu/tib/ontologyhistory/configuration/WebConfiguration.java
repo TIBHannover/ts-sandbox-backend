b0707cbcfdc0d3f3b899920f0f7249d12917bb15
vaninm <mark.vanin@tib.eu>
2024-01-12 12:45:21 +0100

    allowing PUT and other methods in Cors config

--- history/src/main/java/eu/tib/ontologyhistory/configuration/WebConfiguration.java
+++ history/src/main/java/eu/tib/ontologyhistory/configuration/WebConfiguration.java
@@ -14,7 +14,9 @@ public class WebConfiguration  implements WebMvcConfigurer  {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 
 
