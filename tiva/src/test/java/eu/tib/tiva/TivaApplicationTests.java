b376a20d6f8a2c1db7c9383d7f2f50c8ca9f69de
nenadkrdzavac <nenad.krdzavac@gmail.com>
2024-01-19 16:34:10 +0100

    created a tiva mapping service empty project

---
+++ tiva/src/test/java/eu/tib/tiva/TivaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.tib.tiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
