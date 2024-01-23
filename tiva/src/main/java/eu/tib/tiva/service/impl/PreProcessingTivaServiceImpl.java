f309779c135821a8ce12e559653e34344619912b
nenadkrdzavac <nenad.krdzavac@gmail.com>
2024-01-23 12:03:15 +0100

    created pre processing tiva service

---
+++ tiva/src/main/java/eu/tib/tiva/service/impl/PreProcessingTivaServiceImpl.java
@@ -0,0 +1,16 @@
+package eu.tib.tiva.service.impl;
+
+import eu.tib.tiva.model.ProcessedTiva;
+import eu.tib.tiva.service.PreProcessingTivaService;
+
+import java.util.Optional;
+
+public class PreProcessingTivaServiceImpl implements PreProcessingTivaService {
+
+    @Override
+    public ProcessedTiva preProcess(Optional TsTiva, String queryFileLocation, String title) {
+        return null;
+    }
+
+
+}
