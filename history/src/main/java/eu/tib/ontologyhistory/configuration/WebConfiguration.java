f02b4f569fe1030bd1e387d550d5bf8fe219bb2a
vaninm <mark.vanin@tib.eu>
2024-03-20 15:20:44 +0100

    fix

--- history/src/main/java/eu/tib/ontologyhistory/configuration/WebConfiguration.java
+++ history/src/main/java/eu/tib/ontologyhistory/configuration/WebConfiguration.java
@@ -16,8 +16,7 @@ public class WebConfiguration  implements WebMvcConfigurer  {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://ols4ing01.develop.service.tib.eu")
-                .allowedOrigins("https://raw.githubusercontent.com")
+                .allowedOrigins("http://ols4ing01.develop.service.tib.eu", "https://raw.githubusercontent.com")
                 .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 
