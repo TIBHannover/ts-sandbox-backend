8147db69a7cf15c9b52ae7044e43b1ed9e17c3ee
Mark Vanin <mark.vanin@tib.eu>
2024-07-04 14:18:25 +0200

    Federated Query updated to use the org.apache.jena libraries instead of the com.hp.hpl.jena

--- tiva/src/main/java/eu/tib/tiva/utils/FederatedIQuery.java
+++ tiva/src/main/java/eu/tib/tiva/utils/FederatedIQuery.java
@@ -1,134 +1,143 @@
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-import org.apache.jena.atlas.web.auth.PreemptiveBasicAuthenticator;
-import org.apache.jena.atlas.web.auth.SimpleAuthenticator;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+package eu.tib.tiva.utils;
 
-import java.io.IOException;
+import java.net.http.HttpClient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.http.auth.AuthLib;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.sparql.exec.http.QueryExecutionHTTP;
+
+import java.net.Authenticator;
 
 /**
  * The code runs a federated query against TIVA and BACI sparql endpoints.
  */
+@Slf4j
 public class FederatedIQuery {
 
-    public static void main(String[] argv) throws IOException {
+    private static final String ENDPOINT_URL = "https://skynet.coypu.org/coypu-internal/query";
 
-        BasicConfigurator.configure();
+    public static void main(String[] argv) {
 
-        Logger.getRootLogger().setLevel(Level.DEBUG);
+        HttpClient authenticator = authenticate();
 
-        org.apache.jena.atlas.web.auth.HttpAuthenticator authenticator = new PreemptiveBasicAuthenticator(new SimpleAuthenticator("user-name", "pass".toCharArray()), true);
+        try(QueryExecution q = QueryExecutionHTTP.service(ENDPOINT_URL)
+                .query(query())
+                .httpClient(authenticator)
+                .build())
+        {
+            ResultSet results = q.execSelect();
 
-        try(QueryExecution q = QueryExecutionFactory.sparqlService("https://skynet.coypu.org/coypu-internal/query",query(),authenticator)){
+            int i =1;
 
-        ResultSet results = q.execSelect();
+            while(results.hasNext()){
 
-        int i =1;
+                QuerySolution soln = results.next() ;
 
-        while(results.hasNext()){
+                /**
+                 * BACI variable
+                 */
 
-            QuerySolution soln = results.next() ;
-
-            /**
-             * BACI variable
-             */
-
-             RDFNode animport = soln.get("import") ;
-             RDFNode export = soln.get("export");
-             RDFNode productMatch = soln.get("productMatch");
-             Literal productLabel = soln.getLiteral("productLabel");
-             RDFNode amountYear = soln.get("amountYear");
-             Literal amountValue = soln.getLiteral("amountValue");
-             Literal value = soln.getLiteral("value");
-
-
-            /**
-             * TiVA variable
-             */
-
-            Literal vaoImportValue = soln.getLiteral("vaoImportValue");
-            RDFNode vaoImportYear = soln.get("vaoImportYear");
-            RDFNode exIndustryCode = soln.get("exIndustryCode");
-            RDFNode exTradeLocation = soln.get("exTradeLocation");
-            RDFNode importLocation = soln.get("importLocation");
+                 RDFNode animport = soln.get("import") ;
+                 RDFNode export = soln.get("export");
+                 RDFNode productMatch = soln.get("productMatch");
+                 Literal productLabel = soln.getLiteral("productLabel");
+                 RDFNode amountYear = soln.get("amountYear");
+                 Literal amountValue = soln.getLiteral("amountValue");
+                 Literal value = soln.getLiteral("value");
 
 
-            System.out.println(i++ + ".  " +
-                    " | imgr bsci export location: " + exTradeLocation.asNode().getLocalName() +
-                    " | exgr dva export location: " + export.asNode().getLocalName() +
-                    " | imgr export industry code: " + exIndustryCode +
-                    " | imgr bsci import location: " + importLocation.asNode().getLocalName() +
-                     " | exgr dva import location: " + animport.asNode().getLocalName() +
-                    " | exgr dva product match (code): " + productMatch +
-                    " | exgr dva product name: " + productLabel.toString() +
-                     " | exgr dva amount year: " + amountYear.asLiteral().getValue() +
-                     " | exgr  dva amount value: " + amountValue.asLiteral().getValue() +
-                    " | exgr dva trade value: " + value.asLiteral().getValue()+
-                    " imgr bsci value : "+ vaoImportValue.asLiteral().getValue() +
-                    " | imgr bsci year: "+ vaoImportYear.asLiteral().getValue());
+                /**
+                 * TiVA variable
+                 */
 
-        }
+                Literal vaoImportValue = soln.getLiteral("vaoImportValue");
+                RDFNode vaoImportYear = soln.get("vaoImportYear");
+                RDFNode exIndustryCode = soln.get("exIndustryCode");
+                RDFNode exTradeLocation = soln.get("exTradeLocation");
+                RDFNode importLocation = soln.get("importLocation");
 
-        }catch(Exception e) {
 
-            e.printStackTrace();
-        };
+                System.out.println(i++ + ".  " +
+                        " | imgr bsci export location: " + exTradeLocation.asNode().getLocalName() +
+                        " | exgr dva export location: " + export.asNode().getLocalName() +
+                        " | imgr export industry code: " + exIndustryCode +
+                        " | imgr bsci import location: " + importLocation.asNode().getLocalName() +
+                         " | exgr dva import location: " + animport.asNode().getLocalName() +
+                        " | exgr dva product match (code): " + productMatch +
+                        " | exgr dva product name: " + productLabel.toString() +
+                         " | exgr dva amount year: " + amountYear.asLiteral().getValue() +
+                         " | exgr  dva amount value: " + amountValue.asLiteral().getValue() +
+                        " | exgr dva trade value: " + value.asLiteral().getValue()+
+                        " imgr bsci value : "+ vaoImportValue.asLiteral().getValue() +
+                        " | imgr bsci year: "+ vaoImportYear.asLiteral().getValue());
 
-//  ResultSetFormatter.out(System.out, results);
+            }
 
+            }catch(Exception e) {
+
+                e.printStackTrace();
+            };
+
+    }
+
+    private static HttpClient authenticate() {
+        Authenticator authenticator = AuthLib.authenticator("user", "password");
+        return HttpClient.newBuilder()
+                .authenticator(authenticator)
+                .build();
     }
 
     public static String query(){
 
-    String q = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-            "SELECT DISTINCT ?vaoImportValue ?vaoImportYear ?exTradeLocation  ?importLocation " +
-            "?import ?export ?exIndustryCode  ?productMatch ?productLabel ?amountYear ?amountValue ?value  ?sum" +
-            "WHERE " +
-            "{ " +
-            "GRAPH <https://data.coypu.org/trade/baci/> " +
-            "{ " +
-            "?exgrdva rdf:type <https://schema.coypu.org/vtf#ExgrDva> . " +
-            "?exgrdva <https://schema.coypu.org/vtf#hasImport> ?import . " +
-            "?exgrdva <https://schema.coypu.org/vtf#hasExport> ?export ." +
-            "?exgrdva <https://schema.coypu.org/vtf#hasTrade> ?trade . " +
-            "?trade <https://schema.coypu.org/vtf#hasTradeProduct> ?product . " +
-            "?product <http://www.w3.org/2000/01/rdf-schema#label> ?productLabel . " +
-            "?product <http://www.w3.org/2004/02/skos/core#exactMatch> ?productMatch . " +
-            "?trade <https://schema.coypu.org/vtf#hasTradeAmount> ?tradeAmount . " +
-            "?tradeAmount <https://schema.coypu.org/global#hasYear> ?amountYear . " +
-            "?tradeAmount <https://schema.coypu.org/global#hasValue> ?amountValue ." +
-            "?trade <https://schema.coypu.org/vtf#hasTradeValue> ?tradeValue . " +
-            "?tradevalue <https://schema.coypu.org/global#hasValue> ?value . " +
-            "FILTER(str(?amountYear)='2018') . " +
-
-            "} " +
-            "SERVICE <https://tiva.coypu.org/tiva> { " +
-            " {" +
-            "SELECT ?vaoImportValue ?vaoImportYear ?exIndustryCode ?exTradeLocation ?importLocation  " +
-            "WHERE { " +
-            "?imgrbsci a <https://schema.coypu.org/vtf#ImgrBsci> . " +
-            "?imgrbsci <https://schema.coypu.org/global#hasValue> ?vaoImportValue . " +
-            "?imgrbsci <https://schema.coypu.org/global#hasYear> ?vaoImportYear . " +
-            "?imgrbsci <https://schema.coypu.org/vtf#hasExport> ?ex . " +
-            "?ex <https://schema.coypu.org/vtf#hasIndustryCode> ?exIndustryCode . " +
-            "?imgrbsci <https://schema.coypu.org/vtf#hasImport> ?imgrImport . " +
-            "?imgrImport <https://schema.coypu.org/vtf#hasTradeLocation> ?importLocation . " +
-            "?ex rdf:type <https://schema.coypu.org/vtf#Export> . " +
-            "?ex <https://schema.coypu.org/vtf#hasTradeLocation> ?exTradeLocation  . " +
-            "}" +
-            "} " +
-            "} " +
-          "FILTER(str(?import)=str(?importLocation) && str(?export)=str(?exTradeLocation)) . " +
-            "} "+
-            "LIMIT 30";
+    String q = """
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT DISTINCT ?vaoImportValue ?vaoImportYear ?exTradeLocation ?importLocation ?import ?export ?exIndustryCode ?productMatch ?productLabel ?amountYear ?amountValue ?value ?sum
+            WHERE {
+                GRAPH <https://data.coypu.org/trade/baci/> {
+                    ?exgrdva rdf:type <https://schema.coypu.org/vtf#ExgrDva> .
+                    ?exgrdva <https://schema.coypu.org/vtf#hasImport> ?import .
+                    ?exgrdva <https://schema.coypu.org/vtf#hasExport> ?export .
+                    ?exgrdva <https://schema.coypu.org/vtf#hasTrade> ?trade .
+                    ?trade <https://schema.coypu.org/vtf#hasTradeProduct> ?product .
+                    ?product <http://www.w3.org/2000/01/rdf-schema#label> ?productLabel .
+                    ?product <http://www.w3.org/2004/02/skos/core#exactMatch> ?productMatch .
+                    ?trade <https://schema.coypu.org/vtf#hasTradeAmount> ?tradeAmount .
+                    ?tradeAmount <https://schema.coypu.org/global#hasYear> ?amountYear .
+                    ?tradeAmount <https://schema.coypu.org/global#hasValue> ?amountValue .
+                    ?trade <https://schema.coypu.org/vtf#hasTradeValue> ?tradeValue .
+                    ?tradevalue <https://schema.coypu.org/global#hasValue> ?value .
+                    FILTER(str(?amountYear)='2018') .
+                }
+                SERVICE <https://tiva.coypu.org/tiva> {
+                    {
+                        SELECT ?vaoImportValue ?vaoImportYear ?exIndustryCode ?exTradeLocation ?importLocation
+                        WHERE {
+                            ?imgrbsci a <https://schema.coypu.org/vtf#ImgrBsci> .
+                            ?imgrbsci <https://schema.coypu.org/global#hasValue> ?vaoImportValue .
+                            ?imgrbsci <https://schema.coypu.org/global#hasYear> ?vaoImportYear .
+                            ?imgrbsci <https://schema.coypu.org/vtf#hasExport> ?ex .
+                            ?ex <https://schema.coypu.org/vtf#hasIndustryCode> ?exIndustryCode .
+                            ?imgrbsci <https://schema.coypu.org/vtf#hasImport> ?imgrImport .
+                            ?imgrImport <https://schema.coypu.org/vtf#hasTradeLocation> ?importLocation .
+                            ?ex rdf:type <https://schema.coypu.org/vtf#Export> .
+                            ?ex <https://schema.coypu.org/vtf#hasTradeLocation> ?exTradeLocation .
+                        }
+                    }
+                }
+                FILTER(str(?import)=str(?importLocation) && str(?export)=str(?exTradeLocation)) .
+            }
+            LIMIT 30
+            """;
 
         return q;
+
+
 }
+
 
 }
