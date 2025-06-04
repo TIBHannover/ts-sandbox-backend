f1b1dfdd8e8fdfd54f093f75c4b32fd0f361be9b
vaninm <mark.vanin@tib.eu>
2025-06-04 12:52:04 +0200

    disabled scheduled tasks

--- history/src/main/java/eu/tib/ontologyhistory/service/scheduler/OntologyScheduler.java
+++ history/src/main/java/eu/tib/ontologyhistory/service/scheduler/OntologyScheduler.java
@@ -46,8 +46,8 @@ public class OntologyScheduler {
     @EventListener(ApplicationReadyEvent.class)
     @Scheduled(cron = "0 0 7,12,19 * * 1-7")
     public void scheduledOntologyChecks() {
-        scheduledNewTsOntologies();
-        scheduledCheckNewOntologyVersions(false);
+//        scheduledNewTsOntologies();
+//        scheduledCheckNewOntologyVersions(false);
     }
 
     private List<URI> getNewOntologies() {
